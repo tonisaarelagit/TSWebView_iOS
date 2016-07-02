e63dcf3d65c98d25557f835afd7f399f38e5bb8c
Toni Saarela <tn.saarela.dev@gmail.com>
2016-07-02 03:49:05 +0200

    initial commit for all project files

---
+++ TSWebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TSWebView
+//
+//  Created by admin_user on 7/2/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
